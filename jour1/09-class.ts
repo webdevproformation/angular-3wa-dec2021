e7018709cc52049ea7214750a7f5a5e5ba9f3948
Malik H <malik.h@webdevpro.net>
2021-12-13 14:01:56 +0100

    exo 10

--- jour1/09-class.ts
+++ jour1/09-class.ts
@@ -62,3 +62,44 @@ class Etudiant2 {
 
 const e : Etudiant2 = { nom : "ALain" , age : 12 }; 
 
+/*
+Enoncé :
+0 cas pratique créer le fichier 10-exo.ts 
+1 Récupérez la class Produit suivante 
+2 ajoutez les setter et getter pour l'attribut _ref et modifier le constructeur de la class
+
+// ajouter un nouveau membre propriété privée _ref
+// ajouter setter et getter pour ce nouveau membre de la class 
+
+3 créer deux instances de la class Produit avec les arguments suivants :
+
+                name     |     ref
+   produit1 : "Btwin"    | "Decathlon"
+   produit2 : "Peugeot"  |     206
+
+
+4 affichez les deux instances (produit1 et produit2) dans la console du terminal et transpiler le fichier .ts en .js
+
+console.log( )
+*/
+
+class Product {
+    private _name: string;
+
+    constructor(name: string) {
+        this.name = name; // setter assigne une valeur à l'attribut _name
+    }
+
+    // setter
+    set name(name: string) {
+        this._name = name;
+    }
+
+    // getter
+    get name(): string {
+        return this._name;
+    }
+
+}
+
+
