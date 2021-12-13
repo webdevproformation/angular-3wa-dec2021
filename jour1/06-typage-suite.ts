9a11e0ec1a9f9ec8234fa90e5f615abedab4a571
Malik H <malik.h@webdevpro.net>
2021-12-13 11:42:51 +0100

    typage et les fonctions

--- jour1/06-typage-suite.ts
+++ jour1/06-typage-suite.ts
@@ -2,11 +2,39 @@ export {}
 
 interface interfaceA{
     prix : number ,
-    status ?: boolean
+    status ?: boolean // facultative 
 }
-
 
 const a : Array<interfaceA> = [
     {prix : 10 , status : false},
+    {prix : 20}
+];
 
-]
+// fonction classique
+
+function calcul ( a :number , b :number ) :number {
+    return a + b ; 
+}
+
+// fonction fléchée
+
+const puissance = (a :number ) :number =>  {
+    return a ** 2; 
+}
+
+interface interfaceVehicule{
+    nom : string ,
+    dt_Achat ?: Date ,
+    demarrage( modele : string )  : string 
+} 
+
+const voiture : interfaceVehicule = {
+    nom : "Renault",
+    dt_Achat : new Date(),
+    demarrage : function( modele ){
+        return this.nom + " " + modele
+    }
+}
+
+console.log(voiture.nom);
+console.log(voiture.demarrage("Twingo"));
