6880882ea1d88f8b32a92786561aea8ccc8ba8a0
Malik H <malik.h@webdevpro.net>
2021-12-14 14:39:47 +0100

    fin partie opérateurs rxjs

--- jour2-tp/src/app/operateurs.component.ts
+++ jour2-tp/src/app/operateurs.component.ts
@@ -11,9 +11,44 @@ interface interfaceProduit{
     <p *ngFor="let p of p1">{{ p.nom }}</p>
     <hr>
     <button class="btn btn-warning" (click)="onClickBtn2()">Action 2</button>
+    <hr>
+    <button class="btn btn-dark" (click)="onClickBtn3()">Action 3</button>
+    <hr>
+    <button class="btn btn-info" (click)="onClickBtn4()">Action 4</button>
   `
 })
 export class OperateursComponent implements OnInit {
+
+  public onClickBtn4(){
+    // tap opérateur qui permet de récupérer l'information envoyé dans le flux SANS le modifier
+    this.obs$
+    .pipe(
+      tap( data => console.log(data) ),
+      filter( data => {return data.qte > 5 } ),
+      tap( data => console.log(data) ),
+      map( data => { return { id : data.id , nom : data.nom } } ), 
+      tap( data => console.log(data) ),
+      map( data => { return { nom : data.nom.toUpperCase() } } ),
+      tap( data => console.log(data) ),
+      toArray(), 
+      tap( data => console.log(data) ) // traitement AVANT que les données soient affichées dans le composant
+    )
+    .subscribe(  )
+
+  }
+
+  public onClickBtn3(){
+    // enrichir le tableau panier avec une nouvelle information totalTTC 
+    this.obs$
+    .pipe(
+      map(element => {
+        return Object.assign( {} , element , { totalTTC : element.prix * element.qte * 1.2 } )
+      }),
+      toArray()
+    )
+    .subscribe(data => console.log(data));
+  }
+
 
   public onClickBtn2(){
     // si je clique sur le bouton => total de ma commande => somme prix * qte pour chaque produit du panier
