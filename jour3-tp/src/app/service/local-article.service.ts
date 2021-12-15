1a6fbf77d751c6436dd5f14e54e99a1e00b76fbb
Malik H <malik.h@webdevpro.net>
2021-12-15 16:54:23 +0100

    commentaires

--- jour3-tp/src/app/service/local-article.service.ts
+++ jour3-tp/src/app/service/local-article.service.ts
@@ -8,7 +8,16 @@ import { HttpClient } from "@angular/common/http";
 export class LocalArticleService extends LocalDataService {
 
   constructor( http:HttpClient ) {
-    super( http )
+    super( http ) ;
     this.url = "http://localhost:3000/articles"; 
   }
+
 }
+
+// Firebase ??? => sur internet 
+// service de google 
+// facile à mettre 
+// interface visuelle => pour gérer votre base de données en temps réel
+//                    => système d'authentification => base de données spéciales créer un compte 
+//                    et s'authentifier + 
+//                    profil gmail => 
